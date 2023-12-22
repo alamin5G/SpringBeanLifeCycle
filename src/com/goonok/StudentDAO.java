c44d6ede99657aef5af1872859c84af667da65ba
alamin5g <21303134@iubat.edu>
2023-12-22 16:08:13 +0600

    delete jdbc record by id

--- src/com/goonok/StudentDAO.java
+++ src/com/goonok/StudentDAO.java
@@ -11,12 +11,14 @@ public class StudentDAO {
 	 * To write JDBC connection code
 	 */
 	
-	private String driver = "com.mysql.cj.jdbc.Driver";
+	private String driver = "com.mysql.jdbc.Driver";
 	private String url = "jdbc:mysql://localhost:3306/school";
 	private String userName = "root";
 	private String password = "252646";
 	
 	public void selectAllRows() throws ClassNotFoundException, SQLException {
+		System.out.println("Retriving all student data");
+		
 		//Load Driver
 		Class.forName(driver);
 		
@@ -25,7 +27,7 @@ public class StudentDAO {
 		
 		//execute query
 		Statement statement = connection.createStatement();
-		ResultSet resultSet = statement.executeQuery("SELECT * FROM school.student;");
+		ResultSet resultSet = statement.executeQuery("SELECT * FROM school.student");
 		
 		while(resultSet.next()) {
 			int ROOL_NO = resultSet.getInt(1);
@@ -33,5 +35,20 @@ public class StudentDAO {
 			String STUDENT_ADDRESS = resultSet.getString(3);
 			System.out.println(ROOL_NO + "\t" + STUDENT_NAME + "\t" + STUDENT_ADDRESS);
 		}
+	}
+	
+	public void deleteStudentRecord(int ROOL_NO) throws ClassNotFoundException, SQLException {
+		//Load Driver
+		Class.forName(driver);
+		
+		//get a connection
+		Connection connection = DriverManager.getConnection(url, userName, password);
+		
+		//execute Query
+		Statement statement = connection.createStatement();
+		statement.executeUpdate("DELETE FROM school.student WHERE ROOL_NO = " +ROOL_NO);
+		System.out.println("Record Deleted with the ROOL_NO:" + ROOL_NO + " is successful");
+		connection.close();
+		
 	}
 }
--- src/com/goonok/Test.java
+++ src/com/goonok/Test.java
@@ -7,5 +7,7 @@ public class Test {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		StudentDAO studentDAO = new StudentDAO();
 		studentDAO.selectAllRows();
+		studentDAO.deleteStudentRecord(2);
 	}
+	
 }
